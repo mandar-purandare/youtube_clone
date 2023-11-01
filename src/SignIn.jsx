4a82ee7f15b921a6374df1d7a5da56f64177e652
mandar-purandare <mnpsstack@gmail.com>
2023-11-01 22:57:38 +0530

    blue border state

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react'
 import './SignIn.css'
 import google_logo from './images/google_logo.png'
 
 function SignIn() {
 
+    const [border, setBorder] = useState('');
+
+    function blueBorder(e){
+        e.stopPropagation();
+        setBorder(border => border = 'blue-border');
+    }
+
+    function regularBorder(e){
+        e.stopPropagation();
+        setBorder(border => border = '');
+    }
   return (
-    <div className='sign-in-container black-border'>
+    <div className='sign-in-container black-border' onClick={regularBorder}>
         <div className='sign-in-card black-border'>
             <div id='google-logo'>
                 <img src={google_logo}/>
             </div>
             <h3>Sign in</h3>
             <p>to continue to YouTube</p>
-            <div className='email-phone-div'>
+            <div className={`email-phone-div ${border}`} onClick={blueBorder}>
                 <input type='text' placeholder='Email or phone'/>
             </div>
             <p id='forgot-email'>Forgot email?</p>
